7d9d7f2420308565119b3d5f16648b6179268393
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-04-11 15:26:40 -0600

    Add: logger-rate-limiter - submission-2

---
+++ Data Structures & Algorithms/logger-rate-limiter/submission-2.py
@@ -0,0 +1,18 @@
+class Logger:
+
+    def __init__(self):
+        self._m = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message not in self._m:
+            self._m[message] = timestamp
+            return True
+        if timestamp - self._m[message] >= 10:
+            self._m[message] = timestamp
+            return True
+        else:
+            return False
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
