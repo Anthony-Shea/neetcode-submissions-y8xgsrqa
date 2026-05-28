5e418433607862f8ebe959875e5e37d77d030830
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-05-27 19:06:44 -0600

    Add: duplicate-integer - submission-28

---
+++ Data Structures & Algorithms/duplicate-integer/submission-28.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+    m := make(map[int]bool)
+    for _, n := range nums {
+        if m[n] {
+            return true;
+        }
+        m[n] = true
+    }
+    return false;
+}
