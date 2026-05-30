705a04ccf57fc3b2952dbcc42250e790722ed7d7
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-05-30 14:06:41 -0600

    Add: is-anagram - submission-55

---
+++ Data Structures & Algorithms/is-anagram/submission-55.go
@@ -0,0 +1,12 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) { return false }
+    m := make(map[rune]int, 26)
+    for _, c := range s {
+        m[c]++
+    }
+    for _, c := range t {
+        m[c]--
+        if m[c] < 0 { return false }
+    }
+    return true
+}
