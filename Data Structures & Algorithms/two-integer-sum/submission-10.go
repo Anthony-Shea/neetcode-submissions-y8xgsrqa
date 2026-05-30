4966fcec74aa751f81cdc9b1fd7e9e92e10823f8
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-05-30 01:17:32 -0600

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i, num := range nums {
+        diff := target - num
+        if j, ok := m[diff] ; ok {
+            return []int{j, i}
+        }
+        m[num] = i
+    }
+    return []int{}
+}
