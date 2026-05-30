f75797850d8ed8880061a6c8f9b1fe2575ec4c8b
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-05-30 13:20:28 -0600

    Add: duplicate-integer - submission-45

---
+++ Data Structures & Algorithms/duplicate-integer/submission-45.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+    m := make(map[int]struct{})
+    for i := 0; i < len(nums); i++ {
+        if _, ok := m[nums[i]] ; ok {
+            return true
+        }
+        m[nums[i]] = struct{}{}
+    }
+    return false
+}
