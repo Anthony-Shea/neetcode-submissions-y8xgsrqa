9f43bacb06b4dfe65843b2c228a7091e4c3a5e75
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-05-31 17:01:12 -0600

    Add: is-anagram - submission-66

---
+++ Data Structures & Algorithms/is-anagram/submission-66.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        m = defaultdict(int)
+        for c in s:
+            m[c] += 1
+        for c in t:
+            m[c] -= 1
+            if m[c] < 0:
+                return False
+        return True
