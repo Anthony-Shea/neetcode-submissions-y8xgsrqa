d765a2909aa9fa1e30417ad741d6e0674ec48b7e
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-06-07 11:14:29 -0600

    Add: binary-tree-inorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-3.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func inorderTraversal(root *TreeNode) (res []int) {
+    res = []int{}
+    var dfs func(*TreeNode)
+    dfs = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        dfs(node.Left)
+        res = append(res, node.Val)
+        dfs(node.Right)
+    }
+    dfs(root)
+    return
+}
