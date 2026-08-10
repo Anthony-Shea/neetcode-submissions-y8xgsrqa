663e95cae72081e56037cff42f7f91a72fe52507
Anthony Shea <119816999+Anthony-Shea@users.noreply.github.com>
2026-08-10 00:21:33 -0600

    Add: ugly-number - submission-0

---
+++ Data Structures & Algorithms/ugly-number/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        while n != 1:
+            print(n)
+            if n % 5 == 0:
+                n /= 5
+                continue
+            if n % 3 == 0:
+                n /= 3
+                continue
+            if n % 2 == 0:
+                n /= 2
+                continue
+            return False
+        print(n)
+        return True
